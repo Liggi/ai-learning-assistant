52aafd857e6f4f0f86750cf53a1591db5a8ee828
Jason Liggi <jason@gradient-labs.ai>
2025-03-29 18:44:38 +0000

    [refactor] create root article in loader

--- app/routes/learning/$subjectId.tsx
+++ app/routes/learning/$subjectId.tsx
@@ -3,6 +3,7 @@ import { ErrorDisplay } from "@/components/error-display";
 import LearningInterface from "@/components/learning-interface";
 import { getSubject } from "@/prisma/subjects";
 import { getOrCreateLearningMap } from "@/prisma/learning-maps";
+import { createArticle } from "@/prisma/articles";
 import { Logger } from "@/lib/logger";
 
 const logger = new Logger({ context: "LearningRouteLoader" });
@@ -23,21 +24,52 @@ export const Route = createFileRoute("/learning/$subjectId")({
     logger.info("Finding or creating learning map via server fn", {
       subjectId,
     });
-    const learningMap = await getOrCreateLearningMap({
+    let learningMap = await getOrCreateLearningMap({
       data: { subjectId },
     });
 
-    logger.info("Learning map found/created in loader", {
+    logger.info("Initial learning map fetched", {
       learningMapId: learningMap.id,
-      hasArticles: learningMap.articles?.length || 0 > 0,
+      articleCount: learningMap.articles?.length || 0,
     });
+
+    const rootArticleExists =
+      learningMap.articles?.some((article) => article.isRoot) ?? false;
+
+    if (!rootArticleExists) {
+      logger.info("Root article does not exist, creating...", {
+        learningMapId: learningMap.id,
+      });
+      await createArticle({
+        data: {
+          learningMapId: learningMap.id,
+          isRoot: true,
+          content: "",
+        },
+      });
+      logger.info("Root article created, refetching learning map...", {
+        learningMapId: learningMap.id,
+      });
+      learningMap = await getOrCreateLearningMap({
+        data: { subjectId },
+      });
+      logger.info("Learning map refetched after root article creation", {
+        learningMapId: learningMap.id,
+        articleCount: learningMap.articles?.length || 0,
+      });
+    } else {
+      logger.info("Root article already exists", {
+        learningMapId: learningMap.id,
+      });
+    }
 
     return {
       subject,
+      learningMap,
     };
   },
   component: function LearningRoute() {
-    const { subject } = Route.useLoaderData();
+    const { subject, learningMap } = Route.useLoaderData();
     return <LearningInterface subject={subject} />;
   },
   errorComponent: ({ error }) => {
